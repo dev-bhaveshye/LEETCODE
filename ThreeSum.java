2bb1080ee8a2db0f752ed1ce6bd803ca55e46326
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-20 00:25:44 +0530

    Add files via upload

---
+++ ThreeSum.java
@@ -0,0 +1,39 @@
+package CRT_TAE2;
+
+import java.util.*;
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // Sort the array
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicate elements for left and right pointers
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // Increase sum by moving left pointer
+                } else {
+                    right--; // Decrease sum by moving right pointer
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+}
