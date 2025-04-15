581fff2e6d0933e500bad200511eb8d4f4b3cbe0
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-04-15 19:59:57 +0530

    Add files via upload

---
+++ bulbSwitch_319.java
@@ -0,0 +1,20 @@
+package LEETCODE;
+
+public class bulbSwitch_319 {
+    public static void main(String[] args) {
+         int bulbs=3;
+         
+         int i=1;
+         int count =0;
+
+         while(i*i <bulbs){
+            i++;
+            count++;
+         }
+         System.out.println(count);
+         
+
+         
+         
+    }
+}
