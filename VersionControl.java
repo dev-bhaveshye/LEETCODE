9beeab585664831bd3075651705fd04057565a76
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-07-31 23:52:56 +0530

    Add files via upload

---
+++ VersionControl.java
@@ -0,0 +1,14 @@
+package DSA.HackWithInfy.Binary_Search;
+
+public class VersionControl {
+    
+    public static boolean isBadVersion(int n){
+
+        int a = 2 ; 
+        if(n >= a){
+            return true;
+        }
+        return false;
+        
+    }
+}
