3167c1ea0a095084c116a360b4cba34e6d97b2d9
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-09-30 19:30:45 +0530

    Add files via upload

---
+++ Swap_Numbers.java
@@ -0,0 +1,17 @@
+package Capgemini;
+
+public class Swap_Numbers {
+    
+    public static void main(String[] args){
+        int a = 17 ;
+        int b = 18 ; 
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println(a + " ");
+        System.out.println(b + " ");
+    }
+    
+}
