e5b6e132fa96749163b129167bf502f2c44fc4d1
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-09-30 19:30:26 +0530

    Add files via upload

---
+++ Reverse_String.java
@@ -0,0 +1,14 @@
+package Capgemini;
+
+public class Reverse_String {
+    public static void main(String[] args){
+            String s = "booyah";
+            String rev = "";
+
+            for(int i = s.length()-1 ; i >= 0; i--){
+                rev+=s.charAt(i);
+            }
+
+            System.out.println(rev);
+    }
+}
