f8916b6dcaa911075a3f20a4465b3bc5f5d269cc
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-09-30 19:16:20 +0530

    Add files via upload

---
+++ Perfect_number.java
@@ -0,0 +1,20 @@
+package Capgemini;
+
+public class Perfect_number {
+    public static void main(String[] args){
+
+        int num =6 ; 
+        int sum=0;
+        for( int i = 1 ; i <= num/2 ; i++ ){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum == num){
+            System.out.println(true);
+
+        }else{
+            System.out.println(false);
+        }
+    }
+}
